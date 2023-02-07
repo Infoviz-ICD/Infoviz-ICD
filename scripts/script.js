882507914d9f4449ae5f07cc1ee1e6663fc9870a
angstigone <martina.pensalfini@studio.unibo.it>
2023-02-07 15:27:25 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -19,3 +19,14 @@ new Chart("Chart1", {
     }
   }
 });
+
+
+var data = [
+  {
+    x: ['giraffes', 'orangutans', 'monkeys'],
+    y: [20, 14, 23],
+    type: 'bar'
+  }
+];
+
+Plotly.newPlot('myDiv', data);
