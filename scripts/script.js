8f8feeb72b424ebbe100ab17a16d1728587c4e36
angstigone <martina.pensalfini@studio.unibo.it>
2023-02-09 00:20:58 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,18 @@ $(document).ready(function() {
   $("#start").click(function() {
     $("#start").animate({bottom: "-100vh"});
     $("#onTop").animate({top: "-20vh"});
+    document.getElementById("background").style.background = 0;
+    $("#First").animate({top:"-100vh"})
+    $("#CE").prop('id', 'CEvalue');
+    $("#CP").prop('id', 'CPvalue');
   })
-})
+});
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
