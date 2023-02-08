3f52376e694be0e9397ccb56e3857ba8453016b6
Bianca La Manna <92586945+Bianca-LM@users.noreply.github.com>
2023-02-08 17:58:06 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
   $("#start").click(function() {
     $("#start").animate({bottom: "-100vh"});
     $("#onTop").animate({top: "-20vh"});
+    document.getElementById("background").style.background = 0;
   })
-})
+});
