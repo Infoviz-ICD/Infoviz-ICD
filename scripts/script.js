fe14540c475b9b88d64bb5efafc171ac2152adc5
Bianca La Manna <92586945+Bianca-LM@users.noreply.github.com>
2023-02-10 12:53:03 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
   $("#start").click(function() {
     $("#start").animate({bottom: "-100vh"});
@@ -10,11 +11,13 @@ $(document).ready(function() {
   })
 });
 
+
+
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
+  document.getElementById("progression").style.width = scrolled + "%";
 }
