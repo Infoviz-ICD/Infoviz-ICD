e9f67e72b50c156dbb5582627dfd483b91ee7a63
Bianca La Manna <92586945+Bianca-LM@users.noreply.github.com>
2023-02-12 19:14:13 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -24,6 +24,20 @@ $(document).ready(function() {
 
 });
 
+$(document).ready(function() {
+  var trace1 = {
+    x: [2010, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023],
+    y: [1, 1, 11, 5, 2, 3, 15, 2824, 7283, 5355, 6449, 755],
+    type: 'scatter',
+    line: {color:"#f0cb69"}
+  };
+
+
+
+  var data = [trace1];
+
+  Plotly.newPlot('timerange', data);
+})
 
 
 window.onscroll = function() {myFunction()};
